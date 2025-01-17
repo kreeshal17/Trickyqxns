aef80e7145f330c9e3490d68a78c6e2599edfe96
MR BLINDER <kreeshalkarna@gmail.com>
2025-01-18 03:21:10 +0530

    hehe added 4sum

---
+++ medium/Solution.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < n; j++) {
+                if (j != i + 1 && nums[j] == nums[j - 1]) continue;
+                int k = j + 1;
+                int l = n - 1;
+                while (k < l) {
+                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+                    if (sum > target) {
+                        l--;
+                    } else if (sum < target) {
+                        k++;
+                    } else {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k - 1]) k++;
+                        while (k < l && nums[l] == nums[l + 1]) l--;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        // Example input
+        int[] nums = {1, 0, -1, 0, -2, 2};
+        int target = 0;
+
+        // Call fourSum and display the result
+        List<List<Integer>> result = solution.fourSum(nums, target);
+        
+        // Printing the result
+        for (List<Integer> quadruplet : result) {
+            System.out.println(quadruplet);
+        }
+    }
+}
